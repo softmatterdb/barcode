6585f2a129cd90802cf35c3e21bf1f3eb78bb1ec
Adi <adisriram@Adis-MacBook-Air.local>
2025-08-19 12:25:44 -0700

    Corrections to median calculation

--- utils/intensity_distribution.py
+++ utils/intensity_distribution.py
@@ -10,11 +10,11 @@ def stdev(values: np.ndarray, probabilities: np.ndarray):
     return np.sqrt(second_moment - mean ** 2)
 
 def mode(values: np.ndarray, probabilities: np.ndarray):
-    return np.argmax(probabilities)
+    return values[np.argmax(probabilities)]
     
 def median(values: np.ndarray, probabilities: np.ndarray):
     cumulative_probabilities = np.cumsum(probabilities)
-    return values[np.argwhere(cumulative_probabilities >= 0.5)][0]
+    return flatten(values[np.argwhere(cumulative_probabilities >= 0.5)])[0]
 
 def kurtosis(values: np.ndarray, probabilities: np.ndarray):
     def fourth_moment(values: np.ndarray, probabilities: np.ndarray):
@@ -48,8 +48,11 @@ def frame_mode(frame: np.ndarray, bin_number: int):
     counts, values = histogram(frame, bin_number)
     return mode(values, counts)
 
-def histogram(frame: np.ndarray, bin_number: int):
-    count, values = np.histogram(frame, bins=bin_number)
+def histogram(frame: np.ndarray, bin_number: int) -> tuple[np.ndarray, np.ndarray]:
+    if bin_number == 1:
+        values, count = np.unique(frame, return_counts=True)
+    else:
+        count, values = np.histogram(frame, bins=bin_number)
+        values = values[0:-1] + (values[1] - values[0])/2
     count = normalize_counts(count)
-    values = values[0:-1] + (values[1] - values[0])/2
     return count, values
